0f11a0d79ea0417882995a6426924015525f15e0
Kunal <kunaldeshmukh2503@gmail.com>
2020-04-08 01:00:11 -0700

    Random in Noplay, Ladder, Snake

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -2,6 +2,16 @@
 
 
 playerPosition[1]=0
-
+diceRoll=$((RANDOM%6+1))
+randomOption=$((RANDOM%2))
 echo welcome to Snake And Ladder program
-echo $((RANDOM%6+1)) 
+
+if [ $randomOption -eq 0 ]
+then
+	echo "Ladder, moving player ahead by" $dieRoll "Position"
+elif [ $randomOption -eq 1 ]
+then
+	echo "Snake, movin player behind by "$dieRoll" Position"
+else
+ 	echo "you Stay there"
+fi
